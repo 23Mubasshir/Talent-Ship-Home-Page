61301a4773f4bcfc9c51c23732bde82bfe612cf7
23Mubasshir <23mubasshir@gmail.com>
2023-09-05 17:12:48 +0600

    Contact Form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import './App.css'
-import AboutUs from './Sections/AboutUs/AboutUs'
-import Banner from './Sections/Banner/Banner'
-import ContactForm from './Sections/ContactForm/ContactForm'
-import ContactInfo from './Sections/ContactInfo/ContactInfo'
-import Extra1 from './Sections/Extra/Extra1'
-import Footer from './Sections/Footer/Footer'
-import SharedNavbar from './Sections/Navbar/SharedNavbar'
-
+import "./App.css";
+import CustomCursor from "./Components/CustomCursor/CustomCursor";
+import AboutUs from "./Sections/AboutUs/AboutUs";
+import Banner from "./Sections/Banner/Banner";
+import ContactForm from "./Sections/ContactForm/ContactForm";
+import ContactInfo from "./Sections/ContactInfo/ContactInfo";
+import Extra1 from "./Sections/Extra/Extra1";
+import Footer from "./Sections/Footer/Footer";
+import SharedNavbar from "./Sections/Navbar/SharedNavbar";
 
 function App() {
-
   return (
     <>
-    <div>
-    <SharedNavbar></SharedNavbar>
-    <Banner></Banner>
-    <AboutUs></AboutUs>
-    <Extra1></Extra1>
-    <ContactForm></ContactForm>
-    <ContactInfo></ContactInfo>
-    <Footer></Footer>
-    </div>
+      <div>
+        <CustomCursor></CustomCursor>
+          <SharedNavbar></SharedNavbar>
+          <Banner></Banner>
+          <AboutUs></AboutUs>
+          <Extra1></Extra1>
+          <ContactForm></ContactForm>
+          <ContactInfo></ContactInfo>
+          <Footer></Footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
